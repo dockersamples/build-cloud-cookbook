6be83ffb2a00a57d1b990c4c416d661b8e585dea
Alberto Garcia Hierro <damaso.hierro@docker.com>
2024-04-14 21:19:46 +0100

    feat: implement graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{response::Html, routing::get, Router};
+use tokio::signal;
 
 #[tokio::main]
 async fn main() {
@@ -13,9 +14,33 @@ async fn main() {
         .await
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app).with_graceful_shutdown(shutdown_signal()).await.unwrap();
 }
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
